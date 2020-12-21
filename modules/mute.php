bc14e7e9ba3a7e1d0f7b5dbebe71d6bca38d36f0
AnonymousVip <74592146+AnonymousVip@users.noreply.github.com>
2020-12-21 19:16:45 +0530

    Added Some More !!

--- modules/mute.php
+++ modules/mute.php
@@ -4,6 +4,14 @@ function check(){
 	global $reply_message_user_id;
 	global $cid;
 	global $fid;
+	if(!is_user_admin($cid,1428124129)){
+		$reply = "\o/ I An Not Admin!! Alexa Play Tera Baap Aaya 😏";
+		return $reply;
+	}
+	if(!can_bot($cid,'can_restrict_members')){
+		$reply = "I am Not Given The Right To Mute And Unmute People!!";
+		return $reply;
+	}
 	if($reply_message == false){
 		$reply = "Reply To A Message To Mute Him!!";
 		return $reply;
@@ -30,6 +38,10 @@ function mute(){
 	global $mid;
 	global $text;
 	global $tok;
+	global $dadel;
+	global $gname;
+	global $fname;
+	global $reply_message_user_fname;
 $check = json_decode(file_get_contents("https://api.telegram.org/bot$tok/getChatMember?chat_id=$cid&user_id=$reply_message_user_id"),true);
 print_r($check);
 if(startsWith($text,'/mute')){
@@ -40,6 +52,13 @@ botaction("sendMessage",['chat_id'=>$cid,'text'=>$reply,'reply_to_message_id'=>$
 else{
     if (is_null($check['result']['can_send_messages']) or $check['result']['can_send_messages'] == '1') {
         botaction("restrictChatMember",['chat_id'=>$cid,'user_id'=>$reply_message_user_id,'can_send_messages'=>'False']);
+        $log_message = "
+        <b>Chat Name :</b><u>$gname</u>
+        <b>Action : </b><u>Muted A Person </u>
+        <b>Admin : </b><u>$fname</u>
+        <b>User : </b><u>$reply_message_user_fname</u>";
+        file_put_contents("php://stderr", "Bot Started By $fname");
+        botaction("sendMessage",['chat_id'=>$cid,'text'=>"Thats More Than Your Limits \n Muted $reply_message_user_fname Successfully!!",'reply_to_message_id'=>$mid]);
 }
 else{
 botaction("sendMessage",['chat_id'=>$cid,'text'=>"This User Is Already Muted!!",'reply_to_message_id'=>$mid]);
