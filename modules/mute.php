ca5f14e4222dd4f3660e65411daf2cc31ab82ef8
AnonymousVip <74592146+AnonymousVip@users.noreply.github.com>
2020-12-22 16:27:45 +0530

    Aded Un-mute!!

--- modules/mute.php
+++ modules/mute.php
@@ -96,5 +96,72 @@ function mute(){
 	}
 
 }
+
+
+function check_unmute(){
+	global $reply_message;
+	global $reply_message_user_id;
+	global $cid;
+	global $fid;
+	global $mid;
+	global $text;
+	global $tok;
+	global $reply_message_user_fname;
+
+switch ($cid){
+	case(!is_user_admin($cid,1428124129)):
+		$reply = "\o/ I An Not Admin!! Alexa Play Tera Baap Aaya 😏";
+		return $reply;
+	
+	case(!can_bot($cid,'can_restrict_members')):
+		$reply = "I am Not Given The Right To Mute And Unmute People!!";
+		return $reply;
+	
+	case($reply_message == false):
+		$reply = "Reply To A Message To Un-Mute Him!!";
+		return $reply;
+	case(!is_user_admin($cid,$fid)):
+		$reply = "Only Admins Can Execute This Command!!";
+		return $reply;
+	case (can_user($cid,$reply_message_user_id,'can_send_messages') and can_user($cid,$reply_message_user_id,'can_send_media_messages') and can_user($cid,$reply_message_user_id,'can_send_other_messages') and can_user($cid,$reply_message_user_id,'can_add_web_page_previews')):
+	$reply = "This User Already Has The Right To Speak!!";
+	return $reply;
+	default:
+	return null;
+}
+}
+
+function unmute(){
+		global $reply_message;
+	global $reply_message_user_id;
+	global $cid;
+	global $fid;
+	global $mid;
+	global $text;
+	global $tok;
+	global $reply_message_user_fname;
+
+	$reply = check_unmute();
+	if($reply){
+		botaction("sendMessage",['chat_id'=>$cid,'text'=>$reply,'reply_to_message_id'=>$mid]);
+	}
+	else{
+		$unmuting_member = [
+			'chat_id'=>$cid,
+			'user_id'=>$reply_message_user_id,
+			'can_send_messages'=>'True',
+			'can_invite_users'=>'True',
+			'can_pin_messages'=>'True',
+			'can_send_polls'=>'True',
+			'can_change_info'=>'True',
+			'can_send_media_messages'=>'True',
+			'can_send_other_messages'=>'True',
+			'can_add_web_page_previews'=>'True',
+		];
+	        botaction("restrictChatMember",$unmuting_member);
+	botaction("sendMessage",['chat_id'=>$cid,'text'=>"Unmuted Successfully!!",'reply_to_message_id'=>$mid]);
+
+	}
+}
 // $__module_name__ = "Muting";
 ?>
