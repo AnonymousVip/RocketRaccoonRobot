2a390c88b652086f368e5b4a5e956733a232ebd4
AnonymousVip <74592146+AnonymousVip@users.noreply.github.com>
2020-12-23 11:25:06 +0530

    Added /notes and /clear <note_name>

--- modules/notes.php
+++ modules/notes.php
@@ -79,4 +79,73 @@ if(array_key_exists(str_replace('#', '', $text), $notes_list))
 }
 
 }
+
+function clear(){
+	global $reply_message;
+	global $reply_message_user_id;
+	global $reply_message_id;
+	global $cid;
+	global $fid;
+	global $mid;
+	global $text;
+	global $tok;
+	global $gname;
+	global $notes_list;
+	$remove_note_name = get_note_name();
+	$remove_note_id = $notes_list["$remove_note_name"];
+if($remove_note_name)
+{
+	if(!is_user_admin($cid,$fid))
+	{
+		$reply = "Only Admins Can Clear A Note!!";
+		botaction("sendMessage",['chat_id'=>$cid,'text'=>$reply,'reply_to_message_id'=>$mid]);
+	}
+	elseif(!array_key_exists($remove_note_name,$notes_list))
+	{
+		$reply = "There Is No Note By That Name!! How Am I Meant To Delete It 👀";
+		botaction("sendMessage",['chat_id'=>$cid,'text'=>$reply,'reply_to_message_id'=>$mid]);
+	}
+	else{
+		file_get_contents("http://rocket-raccoon-robot.tk/Database/$cid/remove_note.php?name=$remove_note_name&id=$remove_note_id");
+		botaction("sendMessage",['chat_id'=>$cid,'text'=>"Removed <code>$remove_note_name</code> in $gname",'parse_mode'=>'HTML','reply_to_message_id'=>$mid]);
+	}
+}
+else{
+	botaction("sendMessage",['chat_id'=>$cid,'text'=>"Please Give Me A Note Name So That I Am Able To delete It!!",'reply_to_message_id'=>$mid]);
+}
+
+}
+
+function get_all_notes(){
+	global $reply_message;
+	global $reply_message_user_id;
+	global $reply_message_id;
+	global $cid;
+	global $fid;
+	global $mid;
+	global $text;
+	global $tok;
+	global $gname;
+	global $notes_list;
+	$note = '';
+    foreach($notes_list as $key => $value) {
+        $not = "- <code>$key</code>\n";
+        $note .= $not;
+    }
+    $note_message = "Notes In <b>$gname</b> :\n$note\nYou Can Retreive Them By <code>#{note_name}</code>";
+    if($note == '')
+    {
+        $no_notes_dude = [
+            'chat_id'=>$cid,
+			'text'=>"There Are No Notes In <b>$gname</b>",
+			'parse_mode'=>'HTML',
+            'reply_to_message_id'=>$mid
+        ];
+	botaction("sendMessage",['chat_id'=>$cid,'text'=>"There are No Notes In $gname..",'reply_to_message_id'=>$mid]);
+    }
+    else
+    {
+    	botaction("sendMessage",['chat_id'=>$cid,'text'=>"$note_message",'parse_mode'=>'HTML','reply_to_message_id'=>$mid]);
+    }
+}
 ?>
