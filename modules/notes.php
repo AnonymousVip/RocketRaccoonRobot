de9be97cc7f2bab56d43f3dacdd2ea433001efcb
AnonymousVip <74592146+AnonymousVip@users.noreply.github.com>
2020-12-23 19:05:49 +0530

    Update notes.php

--- modules/notes.php
+++ modules/notes.php
@@ -1,122 +1,133 @@
 <?php
-function check_for_notes()
-{
+function get_key_word_and_trigger(){
+	global $texts;
 	global $reply_message;
-	global $reply_message_user_id;
-	global $cid;
-	global $fid;
-	switch ($cid){
-	case(!is_user_admin($cid,$fid)):
-		$reply = "Only Admins Can Execute This Command!!";
-		return $reply;
-	case(!$reply_message):
-		$reply = "Reply To A Message To Add A Note!!";
-		return $reply;
-	default:
-		return null;
-	}
-
-}
-function get_note_name(){
-	global $text;
-	$text_array = explode(' ', $text);
-	if(count($text_array)>=2){
-		$note_name = rawurlencode($text_array['1']);
-		return $note_name;
-	}
-	else{
-		return null;
-	}
-}
-function save(){
-	global $reply_message;
-	global $reply_message_user_id;
-	global $reply_message_id;
-	global $cid;
-	global $fid;
-	global $mid;
-	global $text;
-	global $tok;
-	global $gname;
-	$reply = check_for_notes();
-	if($reply)
+	if(strpos($texts,'"')){
+        $mess_arr = explode(' ', $texts);
+		array_shift($mess_arr);
+        $mess_arr = implode(' ', $mess_arr);
+        $mess_arr = explode('"', $mess_arr);
+		array_shift($mess_arr);
+        $keyword = strtolower($mess_arr['0']);
+        $reply = strtolower($mess_arr['1']);
+    }
+    else{
+        $mess_arr = explode(' ',$texts);
+        array_shift($mess_arr);
+        $keyword = strtolower(array_shift($mess_arr));
+        $reply = implode(' ',$mess_arr);
+    }
+	if($keyword and $reply)
 	{
-		botaction("sendMessage",['chat_id'=>$cid,'text'=>$reply,'reply_to_message_id'=>$mid]);
+		$krray = "$keyword,$reply";
+	return "$krray";
+	}
+	elseif($reply_message)
+	{
+		$krray ="$keyword";
+	return "$krray";
 	}
 	else
 	{
-		$note_name = get_note_name();
-		if($note_name)
-		{
-			 $frwd_reply_msg = json_decode(file_get_contents("https://api.telegram.org/bot$tok/copyMessage?from_chat_id=$cid&chat_id=-1001464778576&message_id=$reply_message_id"),true);
-			$note_id = $frwd_reply_msg['result']['message_id'];
-			file_get_contents("http://rocket-raccoon-robot.tk/Database/$cid/add_note.php?name=$note_name&id=$note_id");
-			$note_added_msg = "Done! Added <code>$note_name</code> in $gname... \n Get that With <code>#$note_name</code>";
-		botaction("sendMessage",['chat_id'=>$cid,'text'=>$note_added_msg,'parse_mode'=>'HTML','reply_to_message_id'=>$mid]);
-		}
-		else
-		{
-			botaction("sendMessage",['chat_id'=>$cid,'text'=>"Note Name Is Missing !!",'reply_to_message_id'=>$mid]);
-		}
+	return false;
 	}
 }
-function hash_get(){
+
+function get_keyword(){
 	global $text;
-	global $notes_list;
-	global $cid;
-	global $mid;
-if(array_key_exists(str_replace('#', '', $text), $notes_list))
-{
-	$tt = str_replace('#', '', $text);
-	$reply_note = $notes_list["$tt"];
-	$send_note_msg = [
-	'from_chat_id'=>'-1001464778576',
-	'chat_id'=>$cid,
-	'message_id'=>$reply_note,
-	'reply_to_message_id'=>$mid
-	];
-	botaction("copyMessage",$send_note_msg);
+	$mess_arr = explode(' ',$text);
+    array_shift($mess_arr);
+    $keyword = strtolower(implode('_',$mess_arr));
+    if($keyword)
+    {
+    	return $keyword;
+    }
+    else
+    {
+    	return false;
+    }
 }
-
-}
-
-function clear(){
+function add_filter(){
 	global $reply_message;
 	global $reply_message_user_id;
 	global $reply_message_id;
 	global $cid;
 	global $fid;
 	global $mid;
-	global $text;
+	global $texts;
 	global $tok;
 	global $gname;
-	global $notes_list;
-	$remove_note_name = get_note_name();
-	$remove_note_id = $notes_list["$remove_note_name"];
-if($remove_note_name)
-{
+
+	$key_trig = get_key_word_and_trigger();
 	if(!is_user_admin($cid,$fid))
 	{
 		$reply = "Only Admins Can Clear A Note!!";
 		botaction("sendMessage",['chat_id'=>$cid,'text'=>$reply,'reply_to_message_id'=>$mid]);
 	}
-	elseif(!array_key_exists($remove_note_name,$notes_list))
+	elseif(!$key_trig)
 	{
-		$reply = "There Is No Note By That Name!! How Am I Meant To Delete It 👀";
+		$reply = "Thats Not The Correct Method Of Adding A Filter !!";
 		botaction("sendMessage",['chat_id'=>$cid,'text'=>$reply,'reply_to_message_id'=>$mid]);
 	}
 	else{
-		file_get_contents("http://rocket-raccoon-robot.tk/Database/$cid/remove_note.php?name=$remove_note_name&id=$remove_note_id");
-		botaction("sendMessage",['chat_id'=>$cid,'text'=>"Removed <code>$remove_note_name</code> in $gname",'parse_mode'=>'HTML','reply_to_message_id'=>$mid]);
+		$key_trig =explode(',', $key_trig);
+		$key_word = $key_trig['0'];
+		$trigger = $key_trig['1'];
+		if($reply_message)
+		{
+	    $frwd_reply_msgs = json_decode(file_get_contents("https://api.telegram.org/bot$tok/copyMessage?from_chat_id=$cid&chat_id=-1001464778576&message_id=$reply_message_id"),true);
+	    $filter_id = $frwd_reply_msgs['result']['message_id'];
+	    file_get_contents('http://rocket-raccoon-robot.tk/Database/'.$cid.'/add_filter.php?kw='.rawurlencode($key_word).'&r='.$filter_id.'');
+	    $done_added = [
+	        'chat_id'=>$cid,
+	        'text'=>"Added Filter $key_word in $gname",
+	        'parse_mode'=>'HTML',
+	        'reply_to_message_id'=>$mid,
+	   ];
+	   botaction("sendMessage",$done_added);
+
+		}
+		else{
+			echo rawurlencode($key_word);
+			$sm = json_decode(file_get_contents('https://api.telegram.org/bot'.$tok.'/sendMessage?chat_id=-1001464778576&text='.rawurlencode($trigger).''),true);
+		    $sid = $sm['result']['message_id'];    
+		    file_get_contents('http://rocket-raccoon-robot.tk/Database/'.$cid.'/add_filter.php?kw='.rawurlencode($key_word).'&r='.$sid.'');
+		    $add_filter_success = [
+		    'chat_id'=>$cid,
+		    'text'=>"Added Filter <code>$key_word</code> in <b>$gname</b>",
+		    'parse_mode'=>'HTML',
+		    'reply_to_message_id'=>$mid,
+		    ];  
+		    botaction("sendMessage",$add_filter_success);
+
+		}
 	}
 }
-else{
-	botaction("sendMessage",['chat_id'=>$cid,'text'=>"Please Give Me A Note Name So That I Am Able To delete It!!",'reply_to_message_id'=>$mid]);
+
+
+function send_filter(){
+	global $text;
+	global $cid;
+	global $mid;
+	global $filters;
+	global $message_dump;
+	foreach ($filters as $fkw => $fri) {
+    $text_array = explode(' ', $text);
+    $fkw = str_replace(' ', '_', $fkw);
+    if(str_contains($text, $fkw)){
+        $reply_filter_id = $filters["$fri"];
+        $send_filter_msg = [
+            'from_chat_id'=>$message_dump,
+            'chat_id'=>$cid,
+            'message_id'=>$fri,
+            'reply_to_message_id'=>$mid,
+        ];
+        botaction("copyMessage",$send_filter_msg);
+    }
+}
 }
 
-}
-
-function get_all_notes(){
+function remove_filter(){
 	global $reply_message;
 	global $reply_message_user_id;
 	global $reply_message_id;
@@ -126,26 +137,30 @@ function get_all_notes(){
 	global $text;
 	global $tok;
 	global $gname;
-	global $notes_list;
-	$note = '';
-    foreach($notes_list as $key => $value) {
-        $not = "- <code>$key</code>\n";
-        $note .= $not;
-    }
-    $note_message = "Notes In <b>$gname</b> :\n$note\nYou Can Retreive Them By <code>#{note_name}</code>";
-    if($note == '')
-    {
-        $no_notes_dude = [
-            'chat_id'=>$cid,
-			'text'=>"There Are No Notes In <b>$gname</b>",
-			'parse_mode'=>'HTML',
-            'reply_to_message_id'=>$mid
-        ];
-	botaction("sendMessage",['chat_id'=>$cid,'text'=>"There are No Notes In $gname..",'reply_to_message_id'=>$mid]);
-    }
-    else
-    {
-    	botaction("sendMessage",['chat_id'=>$cid,'text'=>"$note_message",'parse_mode'=>'HTML','reply_to_message_id'=>$mid]);
-    }
+	global $filters;
+	echo $remove_filter_name = get_keyword();
+	$remove_filter_id = $filters[''.str_replace("_"," ",$remove_filter_name).''];
+if($remove_filter_name)
+{
+	if(!is_user_admin($cid,$fid))
+	{
+		$reply = "Only Admins Can Execute This Command!!";
+		botaction("sendMessage",['chat_id'=>$cid,'text'=>$reply,'reply_to_message_id'=>$mid]);
+	}
+	elseif(!array_key_exists($remove_filter_name,$filters))
+	{
+		$reply = "There Is No Filter By That Name!! How Am I Meant To Stop It 👀";
+		botaction("sendMessage",['chat_id'=>$cid,'text'=>$reply,'reply_to_message_id'=>$mid]);
+	}
+	else{
+		$remove_filter_name = str_replace('_',' ',$remove_filter_name);
+		file_get_contents('http://rocket-raccoon-robot.tk/Database/'.$cid.'/remove_filter.php?kw='.rawurlencode($remove_filter_name).'&id='.$remove_filter_id.'');
+		botaction("sendMessage",['chat_id'=>$cid,'text'=>"Ok! I Will Stop Replying To <code>$remove_filter_name</code> in <b>$gname</b>",'parse_mode'=>'HTML','reply_to_message_id'=>$mid]);
+	}
+}
+else{
+	botaction("sendMessage",['chat_id'=>$cid,'text'=>"Please Give Me A Filter Name So That I Am Able To delete It!!",'reply_to_message_id'=>$mid]);
+}
+
 }
 ?>
